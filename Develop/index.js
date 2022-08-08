0eeb366e4a15e03ad4fb88aea570b93e8e815788
tillzmh <tillzmh@users.noreply.github.com>
2022-08-08 17:14:27 -0400

    first commit

---
+++ Develop/index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [
+    { type:'input',
+    name: 'github',
+    message: "what is your github username"
+}
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();

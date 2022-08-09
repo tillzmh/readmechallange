8c15c5de815bfea67b6f674a5a6ed4dfb8f9f52e
tillzmh <tillzmh@users.noreply.github.com>
2022-08-08 22:31:43 -0400

    created questions and installed npm inquirer package

--- Develop/index.js
+++ Develop/index.js
@@ -3,9 +3,29 @@
 // TODO: Create an array of questions for user input
 const questions = [
     { type:'input',
+    name: 'title',
+    message: "what is your title?",},
+
+    {type: 'input',
+    name: 'description',
+    message: 'what does this application do?',},
+
+    {type: 'input',
+    name: 'installation',
+    message: 'how is this app installed',},
+
+    {type:'input',
+    name: 'usage',
+    message: 'how do you use the application?',},
+
+    {type: 'input',
+    name: 'contributing',
+    message: 'who contributed to this?',},
+
+    {type:'input',
     name: 'github',
-    message: "what is your github username"
-}
+    message: "what is your github username?",},
+
 ];
 
 // TODO: Create a function to write README file
